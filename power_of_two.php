47be85f70ce3776a9c1f21187855d9c4191a7845
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-08-09 18:12:14 +0300

    power of two (daily question)

---
+++ power_of_two.php
@@ -0,0 +1,20 @@
+<?php
+class Solution {
+
+    /**
+     * @param int $n
+     * @return Boolean
+     */
+function isPowerOfTwo($n) {
+        if ($n <= 0) {
+            return false;
+        }
+
+        return (($n & ($n - 1)) == 0);
+    }
+
+}
+
+
+
+?>
